90e432e55a7654cfd411846922b5d8812754fabf
Andre <afaranha@lsd.ufcg.edu.br>
2015-06-14 22:34:08 -0300

    OneViewServerHardwarePublisher.py sends random server hardware and creating OneViewServerProfilePublisher.py for sending random server profile

--- OneViewServerHardwarePublisher.py
+++ OneViewServerHardwarePublisher.py
@@ -1,6 +1,27 @@
 #!/usr/bin/env python
+import json
 import pika
-import sys
+import random
+import uuid
+
+def create_server_hardware_message(cpus, memory_mb, local_gb,
+                                   server_hardware_uri):
+    server_hardware = {'cpus': cpus, 'memory_mb': memory_mb,
+                       'local_gb': local_gb,
+                       'server_hardware_uri':server_hardware_uri }
+    return json.dumps(server_hardware)
+
+
+def create_random_server_hardware_message():
+    cpus = random.randint(1, 60)
+    memory_mb = random.choice([512, 1024, 2048, 4096, 8192, 16384, 32768,
+                               65536])
+    local_gb = random.choice([2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048])
+    server_hardware_uri = str(uuid.uuid4())
+
+    return create_server_hardware_message(cpus, memory_mb, local_gb,
+                                          server_hardware_uri)
+
 
 connection = pika.BlockingConnection(
     pika.ConnectionParameters(host='localhost'))
@@ -9,7 +30,7 @@ channel = connection.channel()
 channel.exchange_declare(exchange='oneview',
                          type='direct')
 
-message = ' '.join(sys.argv[1:]) or 'New Server Hardware'
+message = create_random_server_hardware_message()
 channel.basic_publish(exchange='oneview',
                       routing_key='server_hardware',
                       body=message)
---
+++ OneViewServerProfilePublisher.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+import json
+import pika
+import random
+import uuid
+
+def create_server_profile_message(cpus, disk, ram_mb, name):
+    server_profile = {'cpus': cpus, 'disk': disk, 'ram_mb': ram_mb,
+                       'name': name}
+    return json.dumps(server_profile)
+
+
+def create_random_server_profile_message():
+    cpus = random.randint(1, 60)
+    disk = random.choice([2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048])
+    ram_mb = random.choice([512, 1024, 2048, 4096, 8192, 16384, 32768,
+                               65536])
+    name = str(uuid.uuid4())
+
+    return create_server_profile_message(cpus, disk, ram_mb, name)
+
+
+connection = pika.BlockingConnection(
+    pika.ConnectionParameters(host='localhost'))
+channel = connection.channel()
+
+channel.exchange_declare(exchange='oneview',
+                         type='direct')
+
+message = create_random_server_profile_message()
+channel.basic_publish(exchange='oneview',
+                      routing_key='server_profile',
+                      body=message)
+
+print " [x] Sent server_profile:%r" % (message)
+connection.close()
