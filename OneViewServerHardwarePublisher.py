394173199dbf1cbd20c46e3d6f7aefcffda5944b
Andre <afaranha@lsd.ufcg.edu.br>
2015-06-14 21:31:57 -0300

    Renaming exchange oneview_publisher to oneview, and adding OneViewServerHardwareSubscriber.py

--- OneViewServerHardwarePublisher.py
+++ OneViewServerHardwarePublisher.py
@@ -6,11 +6,11 @@ connection = pika.BlockingConnection(
     pika.ConnectionParameters(host='localhost'))
 channel = connection.channel()
 
-channel.exchange_declare(exchange='oneview_publisher',
+channel.exchange_declare(exchange='oneview',
                          type='direct')
 
 message = ' '.join(sys.argv[1:]) or 'New Server Hardware'
-channel.basic_publish(exchange='oneview_publisher',
+channel.basic_publish(exchange='oneview',
                       routing_key='server_hardware',
                       message=message)
 
--- OneViewServerHardwareSubscriber.py
+++ OneViewServerHardwareSubscriber.py
@@ -1,3 +1,27 @@
 #!/usr/bin/env python
 import pika
 
+connection = pika.BlockingConnection(
+    pika.ConnectionParameters(host='localhost'))
+channel = connection.channel()
+
+channel.exchange_declare(exchange='oneview',
+                         type='direct')
+
+result = channel.queue_declare(exclusive=True)
+queue_name = result.method_queue
+
+channel.queue_bind(exchange='oneview',
+                   queue=queue_name,
+                   routing_key='server_hardware')
+
+print ' [*] Waiting for new Server Hardware. To exit press CTRL+C'
+
+def callback(ch, method, properties, body):
+    print " [x] server_hardware:%r" % (body)
+
+channel.basic_consume(callback,
+                      queue=queue_name,
+                      no_ack=True)
+
+channel.start_consuming()
